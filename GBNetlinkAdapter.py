1216f654b09e46b9362e17a75c792a8ab780e10f
Christopher Friedt <chrisfriedt@gmail.com>
2019-08-11 00:08:55 -0400

    GBNetlinkAdapter.py is working now

--- GBNetlinkAdapter.py
+++ GBNetlinkAdapter.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python2
-
+import select
 import socket
-import struct
-import time
 import threading
 from gnlpy import netlink
 
+import GBOperationMessage
+
 GB_NL_NAME = 'GREYBUS'
-GB_NL_PID = 1        
 
 GBNetlinkCmdAttrList = netlink.create_attr_list_type(
     'GBNetlinkCmdAttrList',
@@ -23,45 +21,119 @@ GBNetlinkMessage = netlink.create_genl_message_type(
 )
 
 class GBNetlinkAdapter(threading.Thread):
-    
-    def __init__(self):
+
+    def __init__(self, netlink_message_callback):
+
         threading.Thread.__init__(self)
+
         self.setupNetlinkSocket()
-        self._greybus_sock = socket.socketpair()
         self._cancellation_sock = socket.socketpair()
-        
+        self._netlink_message_callback = netlink_message_callback
+
         self._should_exit = False
-    
+
     def setupNetlinkSocket(self):
-        
+
         self._netlink_sock_object = netlink.NetlinkSocket()
         family_id = self._netlink_sock_object.resolve_family( GB_NL_NAME )
-        print( '{} family resolves to id {}'.format( GB_NL_NAME, family_id ) )
-        self._netlink_sock_object.port_id = GB_NL_PID
-        print( 'set netlink port id to {}'.format( GB_NL_PID ) )
+        #print( '{} family resolves to id {}'.format( GB_NL_NAME, family_id ) )
         self._netlink_sock = self._netlink_sock_object.sock
-        print( 'netlink socket fd is {}'.format( self._netlink_sock.fileno() ) )
-        
-        # ACK should be corrected to NOACK, same with ACK_REQUEST
-        message = GBNetlinkMessage('GB_NL_C_HD_RESET', flags=netlink.MessageFlags.ACK_REQUEST)
-        
-        self._netlink_sock_object._send( message )
+        #print( 'netlink socket fd is {}'.format( self._netlink_sock.fileno() ) )
+
+        self.resetGreybusHostDevice()
 
     def stop(self):
         self._should_exit = True
-        self._cancellation_sock[ 0 ].send( 'q' )
-    
-    def getGreybusSocket(self):
-        return self._greybus_sock[ 1 ]
-    
+        self._cancellation_sock[ 1 ].send( 'q' )
+
+    def write(self, cport, msg):
+        data = str( msg.pack() )
+
+        message = GBNetlinkMessage(
+            'GB_NL_C_MSG',
+            flags=netlink.MessageFlags.REQUEST,
+            attr_list=GBNetlinkCmdAttrList(GB_NL_A_DATA=data, GB_NL_A_CPORT=cport)
+        )
+
+        self._netlink_sock_object._send( message )
+        #print('Sent netlink message to cport {}: {}'.format(cport, msg))
+
+    def resetGreybusHostDevice(self):
+        # ACK should be corrected to NOACK, same with ACK_REQUEST
+        message = GBNetlinkMessage(
+            'GB_NL_C_HD_RESET',
+            flags=netlink.MessageFlags.REQUEST
+        )
+
+        self._netlink_sock_object._send( message )
+
+    def getSocketsWithData(self):
+
+        sockets_with_data = []
+
+        fd_to_socket = {
+            self._cancellation_sock[ 0 ].fileno(): self._cancellation_sock[ 0 ],
+            self._netlink_sock.fileno(): self._netlink_sock,
+        }
+
+        READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
+        poller = select.poll()
+        poller.register( self._cancellation_sock[ 0 ], READ_ONLY )
+        poller.register( self._netlink_sock, READ_ONLY )
+
+        events = poller.poll( -1 )
+
+        for fd, flag in events:
+
+            s = fd_to_socket[ fd ]
+
+            if flag & (select.POLLHUP | select.POLLERR):
+                msg = '{}: encountered an error on fd {}'.format( self._name, fd )
+                print( msg )
+                raise IOError( msg )
+
+            if s is self._cancellation_sock[ 0 ]:
+                msg = '{}: received data on cancellation fd {}'.format( self._name, fd )
+                self._should_exit = True
+                raise IOError( msg )
+
+            if s is self._netlink_sock:
+
+                sockets_with_data.append( self._netlink_sock )
+                continue
+
+        return sockets_with_data
+
+    def readBytes(self, sock, nbytes):
+
+        sockets_with_data = []
+
+        while sock not in sockets_with_data:
+            sockets_with_data = self.getSocketsWithData()
+
+        data = sock.recv( nbytes )
+
+        return data
+
     def run(self):
         while not self._should_exit:
-            time.sleep( 1 )
 
-if __name__ == '__main__':
-    
-    blah = GBNetlinkAdapter()
-    blah.start()
-    time.sleep( 5 )
-    blah.stop()
-    blah.join()
+            try:
+
+                sockets_with_data = self.getSocketsWithData()
+
+                if self._netlink_sock in sockets_with_data:
+
+                    nl_resp = self._netlink_sock_object._recv()[ 0 ]
+                    attrs = nl_resp.get_attr_list()
+                    cport = attrs.get('GB_NL_A_CPORT')
+                    data = attrs.get('GB_NL_A_DATA')
+                    msg = GBOperationMessage.GBOperationMessage( data )
+
+                    #print('Received netlink message for cport {}: {}'.format(cport, msg))
+
+                    self._netlink_message_callback( cport, msg )
+
+            except:
+                if self._should_exit:
+                    break;
--- GBOperationMessage.py
+++ GBOperationMessage.py
@@ -32,12 +32,11 @@ GB_REQUEST_TYPE_CPORT_SHUTDOWN = 0x00
 class GBOperationMessage(object):
     def __init__(self, data = None ):
         if data is None:
-            data = struct.pack( '<HHBBxx', 0, 0, 0xff, 0 )
-            return
-        
+            data = buffer( struct.pack( '<HHBBBB', 0, 0, 0xff, 0, 0, 0 ) )
+
         if isinstance( data, str ):
             data = buffer( data )
-        
+
         if isinstance( data, tuple ):
 
             if len( data ) == 2:
@@ -48,7 +47,7 @@ class GBOperationMessage(object):
                 self._payload = None
                 self._size = 8
                 return
-                
+
             if len( data ) == 3:
                 # A REQUEST
                 self._operation_id = data[ 0 ]
@@ -57,7 +56,7 @@ class GBOperationMessage(object):
                 self._payload = data[ 2 ]
                 self._size = 8 + len( self._payload )
                 return
-                
+
             if len( data ) == 4:
                 # A RESPONSE
                 self._operation_id = data[ 0 ]
@@ -66,30 +65,30 @@ class GBOperationMessage(object):
                 self._payload = data[ 3 ]
                 self._size = 8 + len( self._payload )
                 return
-            
+
             raise ValueError( 'unsupported tuple length {0}'.format( len( data ) ) )
 
         if isinstance( data, buffer ):
-            x = struct.unpack( '<HHBBxx', data )
+            x = struct.unpack( '<HHBBxx', data[0:8] )
             self._size = x[ 0 ]
             self._operation_id = x[ 1 ]
             self._type = x[ 2 ]
             self._result = x[ 3 ]
             self._payload = None
-            
+
             if len( data ) > 8:
                 self._payload = data[ 8: ]
-            
+
 #             if len( data ) != self._size:
 #                 raise ValueError( "corrupt packet (size: {0}, len: {1})".format( self._size, len( data ) ) )
-            
+
             return
-        
+
         raise ValueError( 'unsupported data type {0}'.format( type( data ) ) )
 
     def __str__(self):
         s = ''
-        x = bytearray( struct.pack( '<HHBBxx', self._size, self._operation_id, self._type, self._result ) )
+        x = bytearray( struct.pack( '<HHBBxx', self._size, self._operation_id, self._type, self._result) )
         for xx in x:
             s += "{0:0{1}x}".format( xx, 2 )
             s += ' '
@@ -98,43 +97,43 @@ class GBOperationMessage(object):
                 s += "{0:0{1}x}".format( int( x ), 2 )
                 s += ' '
         s = s.strip()
-        
+
         return s
-                
+
     def size(self):
         return self._size
-    
+
     def operation_id(self, operation_id = None):
         if operation_id is None:
             return self._operation_id
         self._operation_id = operation_id
-    
+
     def type(self):
         return self._type
 
     def result(self):
-        return self._result 
+        return self._result
 
     def payload(self, data = None):
         if data is None:
             return self._payload
         self._payload = data
         self._size = 8 + len( data )
-    
+
     def isResponse(self):
         return True if ( GB_OP_RESPONSE & self._type ) else False
-    
+
     def pack(self):
-        hdr = str( struct.pack( '<HHBBxx', self._size, self._operation_id, self._type, self._result ) )
+        hdr = str( struct.pack( '<HHBBxx', self._size, self._operation_id, self._type, self._result) )
         pl = str( self._payload )
         msg = hdr + pl
         return buffer( msg )
-    
+
     def response(self, result, payload = None ):
-        
+
         if self.isResponse():
             raise ValueError( "cannot have a response to a response" )
-        
+
         x = self
         x._type |= GB_OP_RESPONSE
         x._result = result
