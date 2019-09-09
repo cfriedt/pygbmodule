81f7e9b10b510abbb252c282d491b7b33c18bca2
Christopher Friedt <chrisfriedt@gmail.com>
2019-09-09 08:20:40 -0400

    fixes flash.py hanging

--- flash.py
+++ flash.py
@@ -23,14 +23,23 @@ def sendMessage(ser, msg):
 
 def getMessage(ser, expected_msg_type):
 
-    data = []
+    data = ''
+    offset = 0
+    remaining = 8
+    recvd = 0
 
-    while True:
-        data = ser.read(8)
-        if 0 == len(data):
+    while remaining > 0:
+        newdata = ser.read(remaining)
+        recvd = len(newdata)
+        if 0 == recvd:
             time.sleep(0.1)
+            continue
+        remaining -= recvd
+        offset += recvd
+        data += newdata
 
-    msg = GBOperationMessage.GBOperationMessage(ser.read(8))
+
+    msg = GBOperationMessage.GBOperationMessage(data)
 
     payload_size = msg.size() - 8
     if payload_size > 0:
@@ -46,9 +55,10 @@ def main(argv):
 
     if len(argv) != 3:
         usage(os.path.basename(argv[0]))
-        return 0
+	return 0
 
-    with serial.Serial(argv[1], 115200, timeout=0.1) as ser:
+    #with serial.Serial(argv[1], 115200, timeout=0.1) as ser:
+    with serial.Serial(argv[1], 115200, timeout=1) as ser:
         print('using ser {}'.format(ser))
 
         with open(argv[2], 'r') as mnfbFile:
@@ -60,14 +70,27 @@ def main(argv):
             payload_size = len(mnfb)
             msg_size = 8 + payload_size
 
+            print('sending manifest..')
+
             msg = GBOperationMessage.GBOperationMessage(struct.pack('<HHBBxx', msg_size, 1, GB_TYPE_MANIFEST_SET, 0))
             msg.payload(mnfb)
 
             sendMessage(ser,msg)
 
+            print('awaiting response')
+
             msg = getMessage(ser, GB_TYPE_MANIFEST_SET | GBOperationMessage.GB_OP_RESPONSE)
 
-            return 0
+            print('received response')
+            
+            r = msg.result()
+            
+            if GBOperationMessage.GB_OP_SUCCESS != r:
+                print('flashing failed {}'.format(r))
+            else:
+                print('OK')
+
+            return r
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
